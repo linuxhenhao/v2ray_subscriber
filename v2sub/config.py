f61666aff637ca310a060c7ed87bea7de6e9f839
Yu Huang <diwang90@gmail.com>
2020-03-30 11:35:06 +0800

    add support for ws connection

--- v2sub/config.py
+++ v2sub/config.py
@@ -3,7 +3,15 @@ from v2sub import utils
 V2RAY_CONFIG_FILE = "/etc/v2ray/config.json"
 
 
-def _get_config(addr: str, port: int, id_: str, client_port=1080) -> dict:
+def _get_config(addr: str, port: int, id_: str, proto: str, ws_path: str=None, client_port=1080) -> dict:
+    stream_settings = {
+        "network": "ws" if proto == "ws" else "tcp",
+        "security": "tls"
+    }
+    if proto == "ws":
+        stream_settings["wsSettings"] = {
+            "path": ws_path   
+        }
     return {
         "inbounds": [
             {
@@ -32,10 +40,7 @@ def _get_config(addr: str, port: int, id_: str, client_port=1080) -> dict:
                         }
                     ]
                 },
-                "streamSettings": {
-                    "network": "tcp",
-                    "security": "tls"
-                }
+                "streamSettings": stream_settings,
             },
             {
                 "protocol": "freedom",
@@ -58,6 +63,6 @@ def _get_config(addr: str, port: int, id_: str, client_port=1080) -> dict:
 
 
 def update_config(node: dict, client_port: int):
-    v2ray_config = _get_config(node['add'], int(node['port']), node['id'],
+    v2ray_config = _get_config(node['add'], int(node['port']), node['id'], node["net"], node["path"],
                                client_port=client_port)
     utils.write_to_json(v2ray_config, V2RAY_CONFIG_FILE)
