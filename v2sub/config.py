1e888fcf88d4b41bbaf9311e83f4a7913df89fcb
huangyu <diwang90@gmail.com>
2020-12-05 13:48:07 +0800

    support tls config

--- v2sub/config.py
+++ v2sub/config.py
@@ -3,15 +3,16 @@ from v2sub import utils
 V2RAY_CONFIG_FILE = "/etc/v2ray/config.json"
 
 
-def _get_config(addr: str, port: int, id_: str, proto: str, ws_path: str=None, client_port=1080) -> dict:
+def _get_config(addr: str, port: int, id_: str, proto: str, tls: str, ws_path: str=None, client_port=1080) -> dict:
     stream_settings = {
         "network": "ws" if proto == "ws" else "tcp",
-        "security": "tls"
     }
     if proto == "ws":
         stream_settings["wsSettings"] = {
-            "path": ws_path   
+            "path": ws_path
         }
+    if tls:
+        stream_settings["security"] = "tls"
     return {
         "inbounds": [
             {
@@ -63,6 +64,7 @@ def _get_config(addr: str, port: int, id_: str, proto: str, ws_path: str=None, c
 
 
 def update_config(node: dict, client_port: int):
-    v2ray_config = _get_config(node['add'], int(node['port']), node['id'], node["net"], node["path"],
-                               client_port=client_port)
+    v2ray_config = _get_config(
+        node['add'], int(node['port']), node['id'], node["net"], node["tls"], node["path"],
+        client_port=client_port)
     utils.write_to_json(v2ray_config, V2RAY_CONFIG_FILE)
